68b10e0614393dcfd8640234d4cdb1384f9e7c14
Administrator <Administrator@DESKTOP-5HGN7MP>
2024-01-24 16:45:03 -0800

    Add project files.

---
+++ Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using CQRS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpGet]
+        [Route("getallpost")]
+        public async  Task<IActionResult> GetAllPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("addpost")]
+        public async Task<IActionResult> AddPost([FromBody] PostModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/PostModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CQRS.Models
+{
+    public class PostModel
+    {
+        public Guid  Id { get; set; }
+
+        public string Title{ get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+    }
+}
